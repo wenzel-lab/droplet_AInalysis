748f2ee10388d01a3d3f4f0f907a95a56bbf3a41
Domingo Veloso <101197947+Chumi-sun@users.noreply.github.com>
2024-05-26 21:47:44 -0400

    more modular main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from ultralytics import YOLO as Yolo
 from get_dimentions import ImageParameters, get_dimentions
 
 path = "predict_test.jpg"
-model = Yolo("current_best_weights.pt")
+weights = "current_best_weights.pt"
+model = Yolo(weights)
 image_info = get_dimentions(image_path=path, model=model, pixel_ratio=1)
 
 print(image_info)
