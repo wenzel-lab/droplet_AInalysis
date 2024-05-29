0923e75f8eab9543f31045a2e4367ea0ce52e802
Domingo Veloso <domingo.veloso@uc.cl>
2024-05-29 01:14:52 -0400

    removed folder

--- training_field/train.py
+++ training_field/train.py
@@ -8,4 +8,4 @@ torch.device("cuda:0")
 if __name__ == "__main__":
     model = YOLO('yolov8n.yaml').load(join("runs","detect","test54","weights","best.pt"))
 
-    results = model.train(data=join("yamls","train_1.yaml"), epochs=50, workers=2, imgsz=640, batch=10, name="test6", device=0, close_mosaic=0)
+    results = model.train(data="train_1.yaml", epochs=50, workers=2, imgsz=640, batch=10, name="test6", device=0, close_mosaic=0)
